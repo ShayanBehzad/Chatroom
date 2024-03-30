106cda2b93c3feb9412d0a2d55662acde86d4a6a
ShayanBehzad <shayan.behzad1380@gmail.com>
2024-03-30 18:14:14 +0330

    implementing websocket

--- main/cunsumers.py
+++ main/cunsumers.py
@@ -1,5 +1,8 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from django.shortcuts import get_object_or_404
+
+from main.models import Conversation, Message
 
 
 class ChatConsumer(WebsocketConsumer):
@@ -13,8 +16,8 @@ class ChatConsumer(WebsocketConsumer):
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
+        user = self.scope['user']
         message = text_data_json["message"]
-        self.user = self.scope['user']
-        print(self.user)
-# shall give rhe self.user to it
-        self.send(text_data=json.dumps({ "message": message}))
+        M = Message(conv=get_object_or_404(Conversation,id=int(self.scope['url_route']['kwargs']['pk'])) , sender=user, content=message)
+        M.save()
+        self.send(text_data=json.dumps({"message": message, "sender":str(user)}))
